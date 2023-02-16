e4d50edf5838df74e12d9b60e9322d4f8d761164
user25 <user25@user25.ru>
2023-02-16 11:03:13 +0500

    Разработка метода для вывода расписания

--- WorkTimetableLibrary/Calculations.cs
+++ WorkTimetableLibrary/Calculations.cs
@@ -8,19 +8,44 @@ namespace WorkTimetableLibrary
 {
     public class Calculations
     {
-        public string AvailablePeriods(TimeSpan[] startTimes,
+        /// <summary>
+        /// Возвращает список свободных временных интервалов (заданного размера) в графике сотрудника для формирования оптимального графика работы сотрудников
+        /// </summary>
+        /// <param name="startTimes">
+        /// Начало
+        /// </param>
+        /// <param name="durations">
+        /// Длительность
+        /// </param>
+        /// <param name="beginWorkingTime">
+        /// Начало рабочего дня
+        /// </param>
+        /// <param name="endWorkingTime">
+        /// Конец рабочего дня
+        /// </param>
+        /// <param name="consultationTim">
+        /// Время приёма
+        /// </param>   
+        /// <returns>
+        /// Список свободных временных интервалов
+        /// </returns>
+        public string[] AvailablePeriods(TimeSpan[] startTimes,
             int[] durations,
             TimeSpan beginWorkingTime,
             TimeSpan endWorkingTime,
             int consultationTim)
         {
+             string[] calc= { };
             TimeSpan arrival = beginWorkingTime;
             TimeSpan departure = endWorkingTime;
             TimeSpan workTime = arrival - departure;
             for (int i = 0; i < workTime.Hours; i++)
             {
-
+                calc[i] = Convert.ToString(Convert.ToInt32 (consultationTim));
+                Console.WriteLine(calc[i]); 
             }
+            return calc;
+            
         }
     }
 }
