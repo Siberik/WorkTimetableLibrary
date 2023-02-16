f9d589cadbd2128ed06a87b658ff5080582c57b5
user25 <user25@user25.ru>
2023-02-16 10:14:24 +0500

    Добавьте файлы проекта.

---
+++ WorkTimetableLibrary/Calculations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkTimetableLibrary
+{
+    public class Calculations
+    {
+        public string AvailablePeriods(TimeSpan[] startTimes,
+            int[] durations,
+            TimeSpan beginWorkingTime,
+            TimeSpan endWorkingTime,
+            int consultationTim)
+        {
+            TimeSpan arrival = beginWorkingTime;
+            TimeSpan departure = endWorkingTime;
+            TimeSpan workTime = arrival - departure;
+            for (int i = 0; i < workTime.Hours; i++)
+            {
+
+            }
+        }
+    }
+}
